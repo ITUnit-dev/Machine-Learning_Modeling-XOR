e94a84e98fa85b0034421d4754d96ab69d1984ed
ITUnitt <burkanow.aleksander@yandex.ru>
2023-02-27 21:48:59 +0300

    add new styles and functions

--- XOR-browser/service_button.js
+++
@@ -1,5 +0,0 @@
-let special_button_hide = document.querySelector(".css-omocl")
-special_button_hide.addEventListener("click", () => {
-    console.log("Worked!")
-    document.querySelector("html").style.marginRight="0px";
-})
---
+++ XOR-browser/service_buttons.js
@@ -0,0 +1,8 @@
+let special_button_hide = document.querySelector(".css-omocl")
+special_button_hide.addEventListener("click", () => {
+    //console.log("Worked!")
+    document.querySelector("html").style.marginRight = "0px";
+})
+
+let special_button_maximize = document.querySelectorAll(".css-1x8bgru")    // доделать перехват и отключение
+special_button_hide.style.display = "none";
